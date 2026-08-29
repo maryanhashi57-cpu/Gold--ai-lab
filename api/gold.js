78586a7a7cc13b1ce3b073cce191cf9c999bce0a
maryanhashi57-cpu <maryanhashi57@gmail.com>
2026-08-28 23:07:43 -0400

    Update gold.js

--- api/gold.js
+++ api/gold.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
 
   try {
 
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
 
     const response = await fetch(
 
-      `https://api.twelvedata.com/price?symbol=XAU/USD&apikey=${apiKey}`
+      `https://api.twelvedata.com/price?symbol=XAU%2FUSD&apikey=${apiKey}`
 
     );
 
